b389aaf8189e38361ae4cdeb9833c870c84d9bdc
jaume-baixas <jaume@biaix.com>
2025-11-10 10:58:38 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { self.clients.claim(); });
-self.addEventListener('fetch', event => { /* opcional: cache aquí */ });
+self.addEventListener('install', e=>{self.skipWaiting();});
+self.addEventListener('activate', e=>{self.clients.claim();});
+self.addEventListener('fetch', e=>{});
