d942479147ae393c4279ffa40895310760fd462e
jaume-baixas <jaume@biaix.com>
2025-11-13 08:34:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trucs-v7'; // Canvia aquest número cada vegada que actualitzis
+const CACHE_NAME = 'trucs-v8'; // Canvia aquest número cada vegada que actualitzis
 const urlsToCache = [
   '.',
   'index.html',
